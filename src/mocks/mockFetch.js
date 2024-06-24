3c35d201785f0b63921817905ca739af9284dd39
Arman Abasian <105098083+Arman-Abbasian@users.noreply.github.com>
2024-06-24 09:33:37 +0330

    mock the post

--- src/mocks/mockFetch.js
+++ src/mocks/mockFetch.js
@@ -3,7 +3,7 @@ const postListResponse =
         {body: "quia et suscipit",id: 1,title: "sunt aut "},
         {body: "suscipit",id: 2,title: "aut "}
     ];
-    
+
 const postSendResponse = {body: "quia et suscipit",id: 1,title: "sunt aut"};
 
 export default async function mockFetch(url) {
@@ -11,6 +11,12 @@ export default async function mockFetch(url) {
         case "https://jsonplaceholder.typicode.com/posts": {
             return {
                 status: 200,
+                json: async () => postListResponse,
+            };
+        }
+        case 'http://localhost:4000/posts',{method: "POST",body:  JSON.stringify(postSendResponse)}: {
+            return {
+                status: 201,
                 json: async () => postListResponse,
             };
         }
